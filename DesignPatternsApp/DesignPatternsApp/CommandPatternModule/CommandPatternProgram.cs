65bf1b4cbde17b711e44a2f9c1e17d63ad83ef58
Leandro Hereñu <leandrohere94@live.com.ar>
2019-05-01 14:31:03 -0300

    Initial configuration of Main Manager and Command Pattern (#1)

---
+++ DesignPatternsApp/DesignPatternsApp/CommandPatternModule/CommandPatternProgram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatternsApp.CommandPatternModule
+{
+    public static class CommandPatternProgram
+    {
+        public static void Init()
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatternsApp/DesignPatternsApp/Program.cs
@@ -0,0 +1,12 @@
+using DesignPatternsApp.CommandPatternModule;
+
+namespace DesignPatternsApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CommandPatternProgram.Init();
+        }
+    }
+}
